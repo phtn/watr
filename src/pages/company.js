baabbbb4936a84af0390befb84692c25ae107744
phtn458 <phtn458@gmail.com>
2019-01-07 21:20:41 -0500

    refined job titles

--- src/pages/company.js
+++ src/pages/company.js
@@ -18,7 +18,7 @@ const people = [
     id: 2,
     title: "The Future of Water Treatment",
     author: "Will Medina",
-    jobTitle: "General Partner",
+    jobTitle: "CTO",
     avatar: Hipster,
     intro: "Advanced Oxidation Process. The invention of the “well” was a revolutionary breakthrough that has been frequently overlooked. This cradle of freshwater was the key to the growth and development of inland communities dating back nearly 10,000 years, that’s 3,000 years before the wheel was even imagined. The well is the first of its kind innovation, which spurred the advancement of a long list of water technologies. Next step has been the creation of (sophisticated) pipeline networks and treatment plants to provide us with this elixir of life, and human beings grew accustomed to an elusive abundance of water, of clean and safe drinking water. But with the passage of time, the already established water purification infrastructure have demonstrated lack of efficiency towards environmental friendliness and in decontaminating emerging water pollutants. The increasing accumulation of emerging pollutants in ground water bodies is a risk to our sustenance and necessitates advancements in water purification technologies.",
     description: "" 
