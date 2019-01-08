f3f192fdbee8baa186ec7f7a24011a7ed1cf5f4a
Kyle Lewis <Liberty@Kyles-MacBook-Pro.local>
2019-01-08 12:42:45 -0500

    yarn add babel-preset-env, the build was failing but compilifine on dev

--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { graphql } from 'gatsby';
+// import React from 'react'
+// import { graphql } from 'gatsby';
 
-export default function Template({data}) {
-  const { markdownRemark: post } = data
-  console.log(typeof post)
-  const { title } = post.frontmatter
-  return(
-    <>
-      <h3>{title}</h3>
-      <div dangerouslySetInnerHTML={{__html: post.html}}></div>
-    </>
-  )
-}
+// export default function Template({data}) {
+//   const { markdownRemark: post } = data
+//   console.log(typeof post)
+//   const { title } = post.frontmatter
+//   return(
+//     <>
+//       <h3>{title}</h3>
+//       <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+//     </>
+//   )
+// }
 
-export const postQ = graphql`
-  query PostListByPath($path: String!) {
-  markdownRemark(frontmatter: {path: {eq: $path } }) {
-      html
-      frontmatter {
-        path
-        title
-      }
-    }
-  }
-`
+// export const postQ = graphql`
+//   query PostListByPath($path: String!) {
+//   markdownRemark(frontmatter: {path: {eq: $path } }) {
+//       html
+//       frontmatter {
+//         path
+//         title
+//       }
+//     }
+//   }
+// `
