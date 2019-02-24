bdac80cee76e291760b2ecab0c0abc31fc5c1c4b
Kyle Lewis <Liberty@Kyles-MacBook-Pro.local>
2019-02-24 14:48:01 -0500

    polished reason component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,147 @@
-import React from 'react'
-import Layout from '../components/layout';
-import ClearWater from '../assets/clear-water.mp4';
-import Helmet from 'react-helmet'
-import Newsletter from '../components/newsletter';
-import Fade from 'react-reveal'
-import { FixedSizeList as List } from 'react-window';
-
-
-
-
+import React, { useState, useEffect } from "react";
+import Layout from "../components/layout";
+import ClearWater from "../assets/clear-water.mp4";
+import Helmet from "react-helmet";
+import Newsletter from "../components/newsletter";
+import Fade from "react-reveal";
+import { FixedSizeList as List } from "react-window";
 
 const styles = {
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%"
   },
   landingContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 50
   },
-  
+
   bridge: {
     minHeight: 100,
     backgroundColor: "#fff",
     // border: '1px solid red',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   }
-}
+};
 const Landing = props => {
- 
-  
   return (
     <div style={styles.landingContainer}>
-      <video id='landingVideo' muted autoPlay loop width='100%'>
+      <video id="landingVideo" muted autoPlay loop width="100%">
         <source src={ClearWater} />
       </video>
-      
     </div>
-  )
-}
+  );
+};
+
+const reasons = [
+  "Greatly improves immune system",
+  "Prevents Gastrointestinal diseases",
+  "Improved Hydration Levels"
+]
 
 const Column = ({ index, style }) => (
   <div style={style}>
-    <div style={{ height: 100, padding: 10, backgroundColor: '#34CDFA', textAlign: 'center'}}>Reason {index + 1}</div>
+    <div
+      style={{
+        height: 100,
+        padding: 10,
+        backgroundColor: "rgba(52,205,250, 0.3)",
+        textAlign: "center"
+      }}
+    >
+      <div
+        style={{
+          // border: "1px solid #ccc",
+          borderRadius: 5,
+          boxShadow: "0 2px 4px 0 rgba(14,30,37,.12)",
+          backgroundColor: "#eee",
+          height: 100,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <p style={{fontFamily: 'Open Sans, sans-serif'}}>{reasons[index]}</p>
+      </div>
+    </div>
   </div>
 );
- 
+
 const Reasons = props => {
-  const {width} = props
   return (
     <List
       direction="horizontal"
       height={120}
-      itemCount={1000}
-      itemSize={300}
-      width={400}
+      itemCount={reasons.length}
+      itemSize={props.width * 0.8}
+      width={props.width}
     >
       {Column}
     </List>
-  )
-}
+  );
+};
 
 const Essential = props => {
   return (
-    <div style={{textAlign: 'center', padding: 15}}>
+    <div style={{ textAlign: "center", padding: 15 }}>
       <Fade top>
-        <h1 style={{fontSize: 24, fontFamily: 'Quicksand', fontWeight: 'bolder'}}>Clean Drinking Water is essential to your Health.</h1>
-        <p style={{fontFamily: 'Open Sans, sans-serif', color: '#444'}}>and here are the reasons why:</p>
+        <h1
+          style={{
+            fontSize: 24,
+            fontFamily: "Quicksand",
+            fontWeight: "bolder"
+          }}
+        >
+          Clean Drinking Water is essential to your Health.
+        </h1>
+        <p style={{ fontFamily: "Open Sans, sans-serif", color: "#444" }}>
+          and here are the reasons why:
+        </p>
       </Fade>
-
     </div>
-  )
-}
+  );
+};
 
 const Bridge = props => {
   // const { width } = props
   // console.log(width)
-  return(
-    <div style={styles.bridge}>
-        {props.component}
-    </div>
-  )
-}
+  return <div style={styles.bridge}>{props.component}</div>;
+};
 
 export default function Index() {
+  const [width, setWidth] = useState(
+    typeof global !== "undefined" && global.innerWidth
+  );
+
+  useEffect(() => {
+    const handleWidthResize = () => setWidth(global.innerWidth);
+
+    return () =>
+      typeof global !== "undefined" &&
+      global.removeEventListener("resize", handleWidthResize);
+  });
+
   return (
     <>
       <Helmet>
-        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700i|Quicksand|Roboto:100" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css?family=Playfair+Display:700i|Quicksand|Roboto:100"
+          rel="stylesheet"
+        />
       </Helmet>
 
       <Layout title={`William Wallace Water`}>
-  
-        <Landing/>
-        <Bridge component={<Essential/>}/>
-        <Bridge component={<Reasons/>}/>
-        <Bridge component={<Newsletter/>}/>
-
+        <Landing />
+        <Bridge component={<Essential />} />
+        <Bridge component={<Reasons width={width} />} />
+        <Bridge component={<Newsletter />} />
       </Layout>
     </>
-  )
+  );
 }
