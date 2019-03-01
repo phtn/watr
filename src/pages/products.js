43cad4e0224a528e122d9dd38c02b3576d7bd004
phtn458 <phtn458@gmail.com>
2019-03-01 02:35:42 -0500

    removed products folder

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,57 +1,36 @@
-import React from "react";
-import Layout from "../components/layout";
-import BlogList from "../components/blog-list.rc";
-import Pitcher from "../assets/filter.svg";
-import { graphql } from "gatsby";
-import Helmet from 'react-helmet'
+// import React from 'react'
+// import Helmet from 'react-helmet'
+// import Layout from "../components/layout";
+// import ProductList from "../components/product-list";
+// import berkeyLogo from '../assets/bbwf-logo.png'
+// import enagicLogo from '../assets/enagic-logo.svg'
+// import royalBerkey from '../assets/royal-berkey.jpeg'
+// import rainsoftLogo from '../assets/rainsoft-logo.png'
+// import ionfaucetLogo from '../assets/ionfaucet-logo.png'
+// import k8 from '../assets/k8-2.jpg'
+// import Filter from '../assets/filter2.svg'
+// import ultre from '../assets/ultre1.jpg'
+// import stageIon from '../assets/3stage-ion.jpg'
 
-const Products = ({ data, pad }) => {
-  const { allMarkdownRemark: post } = data;
+// const products = [
+//   { id: 0, title: "Big Berkey Water Filters", logo: berkeyLogo, image: royalBerkey, description: "Perfect for college students & renters.", link: "https://www.bigberkeywaterfilters.com/berkey-water-filters.html" },
+//   { id: 1, title: "Enagic Kangen Water", logo: enagicLogo, image: k8, description: "Upgrade your Home.", link: "https://www.enagic.com/?c=product-k8" },
+//   { id: 2, title: "Ion Faucet", logo: ionfaucetLogo, image: stageIon, description: "Simple Filtration System that works!", link: "https://www.ionfaucet.com/category-s/1477.htm" },
+//   { id: 3, title: "RainSoft", logo: rainsoftLogo, image: ultre, description: "Get the Ultimate Upgrade!", link: "https://www.rainsoft.com/premium-water-conditioning-systems/" },
+// ];
 
-  return (
-    <>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700i|Quicksand|Roboto:100" rel="stylesheet"/>
-      </Helmet>
-      <Layout title="Products">
-        <BlogList
-          items={post.edges}
-          headerTitle={`Products`}
-          title={""}
-          buttonTitle={`Read More`}
-          pad={pad}
-          icon={Pitcher}
-        />
-      </Layout>
-    </>
-  );
-};
-export default Products;
 
-export const getBlogs = graphql`
-  query ProductsList {
-    allMarkdownRemark(
-      sort: { fields: [frontmatter___id], order: DESC }
-      limit: 5
-    ) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            path
-            author
-            createdAt
-            description
-            avatar
-            readTime
-            updatedAt
-            source
-            id
-          }
-        }
-      }
-    }
-  }
-`;
+// const Products = () => {
+//   return (
+//     <>
+//       <Helmet>
+//         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700i|Quicksand|Roboto:100" rel="stylesheet"/>
+//       </Helmet>
+
+//       <Layout title={`Products`}>
+//         <ProductList items={products} headerTitle={`All Products`} buttonTitle={`View All Products`} icon={Filter}/>
+//       </Layout>
+//     </>
+//   )
+// }
+// export default Products;
